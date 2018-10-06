a0d250b9382f54c6ba4c250ffdfa6d5b61610054
Alberto <bertonc98@gmail.com>
2018-10-06 14:54:18 +0200

    up

--- cifrario.c
+++ cifrario.c
@@ -1,6 +1,34 @@
 #include<stdio.h>
+#include<ctype.h>
 
 int main(){
+int key;
+char c, pulisci_buffer;
 
+scanf(" %d", &key);
+pulisci_buffer=getchar();
+
+while( (c=getchar())!='\n'){
+	if(islower(c)){
+		if((c+key)>'z'){
+			printf("%c", 'a'+((c-'a')+key)%26);
+		}
+		else
+			printf("%c", c+key);
+		
+	}
+	else if(isupper(c)){
+		if((c+key)>'Z'){
+			printf("%c", 'A'+((c-'A')+key)%26);
+		}
+		else
+			printf("%c", c+key);
+		
+	}
+	else
+		printf("%c",c);
+}
+
+printf("\n");
 return 0;
 }
